b6a4c9002a1d5a719dd06942740a70cc2a55cd00
Ivan Gurkov <ivangurkoff@gmail.com>
2026-01-10 14:09:05 +0300

    finished 2.4

---
+++ 2ch/2.4 Multiple Representations for Abstract Data/2.75.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+#| Implement the constructor make-from-mag-ang in
+message-passing style. This procedure should be analogous to
+the make-from-real-imag procedure given above. |#
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond
+      [(eq? op 'magnitude) x]
+      [(eq? op 'angle) y]
+      [(eq? op 'real-part) (* x (cos y))]
+      [(eq? op 'imag-part) (* x (sin y))]
+      [else (error "Unknown op:
+            MAKE-FROM-MAG-ANG" op)]))
+  dispatch)
